2f99904227dd4de10169c8f980c7f84fb3a75a79
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-10-04 21:54:34 +0530

    menu for mobile, sidebar completed

--- components/Providers/modal-provider.tsx
+++ components/Providers/modal-provider.tsx
@@ -8,7 +8,9 @@ import { EditServerModal } from "@/components/modals/edit-server-modal"
 import { MembersModal } from "@/components/modals/members-modal"
 import { CreateChannelModal } from "@/components/modals/create-channel-modal"
 import { LeaveServerModal } from "@/components/modals/leave-server-modal"
-import { DeleteServerModal } from "../modals/delete-server-modal"
+import { DeleteServerModal } from "@/components/modals/delete-server-modal"
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal"
+import { EditChannelModal } from "@/components/modals/edit-channel-modal"
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false)
@@ -28,8 +30,10 @@ export const ModalProvider = () => {
             <EditServerModal />
             <MembersModal />
             <CreateChannelModal />
-            <LeaveServerModal/>
+            <LeaveServerModal />
             <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModal />
         </>
     )
 }
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -9,7 +9,7 @@ import { NavigationItem } from '@/components/navigation/navigation-item';
 import { ModeToggle } from '@/components/mode-toggle';
 import { UserButton } from '@clerk/nextjs';
 
-const NavigationSidebar = async () => {
+export const NavigationSidebar = async () => {
     const profile = await currentProfile();
 
     if (!profile) return redirect('/')
@@ -60,5 +60,3 @@ const NavigationSidebar = async () => {
         </div>
     )
 }
-
-export default NavigationSidebar
