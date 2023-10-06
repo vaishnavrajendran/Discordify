bdae51d73b3cdc9e3e906267dc562617aa8e02d6
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-10-06 20:35:10 +0530

    @ changed to ./

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { cn } from '@/lib/utils'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ModalProvider } from '@/components/providers/modal-provider'
-import { SocketProvider } from '@/components/providers/socket-provider'
-import { QueryProvider } from '@/components/providers/query-provider'
+import { ThemeProvider } from '../components/providers/theme-provider'
+import { ModalProvider } from '../components/providers/modal-provider'
+import { SocketProvider } from '../components/providers/socket-provider'
+import { QueryProvider } from '../components/providers/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
